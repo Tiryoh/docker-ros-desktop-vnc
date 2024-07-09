040f805b9b0c9dba963ac88d533d518fa95cecf2
Daisuke Sato <tiryoh@gmail.com>
2024-07-09 12:56:18 +0900

    fix: Add workaround for issue when image is created with "docker commit" (#57)

--- melodic/entrypoint.sh
+++ melodic/entrypoint.sh
@@ -42,6 +42,17 @@ VNCRUN_PATH="$HOME/.vnc/vnc_run.sh"
 cat << EOF > "$VNCRUN_PATH"
 #!/bin/sh
 
+# Workaround for issue when image is created with "docker commit".
+# Thanks to @SaadRana17
+# https://github.com/Tiryoh/docker-ros2-desktop-vnc/issues/131#issuecomment-2184156856
+
+if [ -e /tmp/.X1-lock ]; then
+    rm -f /tmp/.X1-lock
+fi
+if [ -e /tmp/.X11-unix/X1 ]; then
+    rm -f /tmp/.X11-unix/X1
+fi
+
 if [ $(uname -m) = "aarch64" ]; then
     LD_PRELOAD=/lib/aarch64-linux-gnu/libgcc_s.so.1 vncserver :1 -fg -geometry 1920x1080 -depth 24
 else
